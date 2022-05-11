738f3f5f7cd99203d324cac46bce4abc60dc5d18
SeoWo0 <97532242+SeoWo0@users.noreply.github.com>
2022-05-11 21:50:46 +0900

    유닛 선택, 드래그 시스템 1차
    마우스로 유닛 선택, 해제
    드래그로 유닛 선택, 해제
    1차

---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject unitMarker;
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake() {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void SelectUnit()
+    {
+        unitMarker.SetActive(true);
+    }
+
+    public void DeSelectUnit()
+    {
+        unitMarker.SetActive(false);
+    }
+
+    public void MoveTo(Vector3 targetPos)
+    {
+        navMeshAgent.SetDestination(targetPos);
+    }
+
+    
+}
